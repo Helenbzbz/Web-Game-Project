33aad15ab724a4178661950991e3c86c4c92dd02
Helenbzbz <97910472+Helenbzbz@users.noreply.github.com>
2022-04-29 19:17:42 -0400

    update

--- app.py
+++ app.py
@@ -53,8 +53,79 @@ def submit4():
 
 @app.route('/Q5',methods = ['GET','POST'],endpoint = 'submit5')
 def submit5():
-    return render_template('Q5.html',prompt = Q5.prompt)
+    if request.method == 'POST':
+        l = request.form.getlist('mycheckbox')
+        result = ','.join(l)
+        f = open("result.txt", "a")
+        f.write('\n')
+        f.write(result)
+        return redirect('/Q6')
+    else:
+        return render_template('Q5.html',prompt = Q5.prompt)
 
+@app.route('/Q6',methods = ['GET','POST'],endpoint = 'submit6')
+def submit6():
+    if request.method == 'POST':
+        l = request.form.getlist('mycheckbox')
+        result = ','.join(l)
+        f = open("result.txt", "a")
+        f.write('\n')
+        f.write(result)
+        return redirect('/Q7')
+    else:
+        return render_template('Q6.html',prompt = Q6.prompt)
+
+@app.route('/Q7',methods = ['GET','POST'],endpoint = 'submit7')
+def submit7():
+    if request.method == 'POST':
+        l = request.form.getlist('mycheckbox')
+        result = ','.join(l)
+        f = open("result.txt", "a")
+        f.write('\n')
+        f.write(result)
+        return redirect('/Q8')
+    else:
+        return render_template('Q7.html',prompt = Q7.prompt)
+
+@app.route('/Q8',methods = ['GET','POST'],endpoint = 'submit8')
+def submit8():
+    if request.method == 'POST':
+        f = open("result.txt", "a")
+        f.write('\n')
+        f.write(request.form['option'])
+        return redirect('/Q9')
+    else:
+        return render_template('Q8.html',prompt = Q8.prompt)
+
+@app.route('/Q9',methods = ['GET','POST'],endpoint = 'submit9')
+def submit9():
+    if request.method == 'POST':
+        f = open("result.txt", "a")
+        f.write('\n')
+        f.write(request.form['option'])
+        return redirect('/Q10')
+    else:
+        return render_template('Q9.html',prompt = Q9.prompt)
+
+@app.route('/Q10',methods = ['GET','POST'],endpoint = 'submit10')
+def submit10():
+    if request.method == 'POST':
+        f = open("result.txt", "a")
+        f.write('\n')
+        f.write(request.form['option'])
+        return redirect('/Q11')
+    else:
+        return render_template('Q10.html',prompt = Q10.prompt)
+
+@app.route('/Q11',methods = ['GET','POST'],endpoint = 'submit11')
+def submit11():
+    if request.method == 'POST':
+        f = open("result.txt", "a")
+        f.write('\n')
+        f.write(request.form['option'])
+        return redirect('/result')
+    else:
+        return render_template('Q11.html',prompt = Q11.prompt)
 
 if __name__ == "__main__":
     app.run()
