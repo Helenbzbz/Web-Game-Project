4f0e4a88fabe3d425bd3b9a2e60c041cb34602dd
Helenbzbz <97910472+Helenbzbz@users.noreply.github.com>
2022-04-30 09:48:33 -0400

    update

---
+++ score.py
@@ -0,0 +1,7 @@
+# This code wll read the result file and return the days an individual can survive
+
+from single import Q1,Q2,Q3,Q4,Q8,Q9,Q10,Q11
+from multiple import Q5, Q6, Q7
+
+def score():
+    f = 
